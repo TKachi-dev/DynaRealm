e40b368063e2fca8dc1e52a2f6141c538dca4f34
Takuma Kachi <tk1205.626@gmail.com>
2026-05-06 20:29:42 +0900

    Add calendar grid with 42 days and ViewModel logic

---
+++ ViewModels/CalendarDayViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynaRealm.ViewModels
+{
+    public class CalendarDayViewModel
+    {
+        public DateTime Date {  get; set; }
+
+        public int DayNumber => Date.Day;
+
+        public bool IsCurrentMonth {  get; set; }
+
+        public bool IsToday => Date.Date == DateTime.Today;
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -10,6 +10,9 @@ public partial class MainWindowViewModel : ViewModelBase
     // タブ一覧
     public ObservableCollection<Tab> Tabs { get; set; } = new();
 
+    // カレンダー日付一覧
+    public ObservableCollection<CalendarDayViewModel> CalendarDays { get; set; } = new();
+
     // 月表示
     public string CurrentMonthText { get; set; } =
         $"{DateTime.Today.Year}年{DateTime.Today.Month}月";
@@ -28,6 +31,29 @@ public partial class MainWindowViewModel : ViewModelBase
         foreach (Tab tab in tabs)
         {
             Tabs.Add(tab);
+        }
+
+        // カレンダー日付生成
+        CreateCalendarDays(DateTime.Today);
+    }
+
+    // カレンダー日付生成処理
+    private void CreateCalendarDays(DateTime targetDate)
+    {
+        CalendarDays.Clear();
+
+        var firstDayOfMonth = new DateTime(targetDate.Year, targetDate.Month, 1);
+        var startDate = firstDayOfMonth.AddDays(-(int)firstDayOfMonth.DayOfWeek);
+
+        for (int i = 0; i < 42; i++)
+        {
+            var date = startDate.AddDays(i);
+
+            CalendarDays.Add(new CalendarDayViewModel
+            {
+                Date = date,
+                IsCurrentMonth = date.Month == targetDate.Month
+            });
         }
     }
 }
