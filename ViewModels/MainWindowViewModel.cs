ea8e100377ceece997c589e75679cafe304d496b
Takuma Kachi <tk1205.626@gmail.com>
2026-05-06 19:53:08 +0900

    Add calender header and weekdays

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using DynaRealm.Models;
 using DynaRealm.Services;
 
@@ -6,7 +7,18 @@ namespace DynaRealm.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    // タブ一覧
     public ObservableCollection<Tab> Tabs { get; set; } = new();
+
+    // 月表示
+    public string CurrentMonthText { get; set; } =
+        $"{DateTime.Today.Year}年{DateTime.Today.Month}月";
+
+    // 曜日
+    public string[] WeekDays { get; set; } =
+    {
+        "日", "月", "火", "水", "木", "金", "土"
+    };
 
     public MainWindowViewModel()
     {
